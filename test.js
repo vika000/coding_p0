d6962f6e6862a327bc6ff20ea4202fd3d7bcd8b6
andy zero <zero@andys-bad-boy.local>
2019-01-16 14:26:01 -0800

    changed the online file completely and made it actually have something on it ... also frankie, nice freind in my class, gave me some invaulbe code .... the createCanvas(windowW .... etc ) ... this code makes the screen adaptable to any screen veiwing it!

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 function setup() {
+	createCanvas(windowWidth, windowHeight);
 
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+	window.onresize = function(){createCanvas(windowWidth, windowHeight)};
+	
+	if (mousePressed) {
+		fill(0);
+	} else {
+		fill(255);
+	}
+  ellipse(mouseX, mouseY, 80, 80);
 }
