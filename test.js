7a7fa4386ff75a0bd7729d43ec8657474eaa6446
andy zero <zero@andys-bad-boy.local>
2019-01-19 22:46:04 -0800

    changed code from boring circles that change with mouse click to a tourus with directional lite

--- test.js
+++ test.js
@@ -1,16 +1,31 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(255);
+	
 
 }
 
 function draw() {
+	background(0)
 	window.onresize = function(){createCanvas(windowWidth, windowHeight)};
+	let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
 
-	if (mouseIsPressed) {
-		fill(200, 200, 200, 200);
-	} else {
-		fill(255);
-	}
-  ellipse(mouseX, mouseY, 30, 45);
+  ambientLight(60, 60, 60);
+  pointLight(255, 255, 255, locX, locY, 100);
+ 
+  push();
+  translate(-width / 4, height / 4, 0);
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  ambientMaterial(250);
+  torus(80, 20, 64, 64);
+  pop();
+
+	//if (mouseIsPressed) {
+		//fill(200, 200, 200, 200);
+	//} else {
+		//fill(255);
+	//}
+  //ellipse(mouseX, mouseY, 30, 45);
 }
